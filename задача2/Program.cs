61648928f3909a19949a401b527501ba87c687b4
Ilya Grekov <grekovis93@gmail.com>
2022-03-15 16:08:13 +0700

    сохранение

---
+++ задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите числа a и b");
+Console.Write("Введите a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b = ");
+int b = int.Parse(Console.ReadLine());
+int n = 1;
+for (int i = 1; i <= b; i++)
+{
+    n = n * a;
+}
+Console.WriteLine(a + " в степени " + b + " = " + n);
