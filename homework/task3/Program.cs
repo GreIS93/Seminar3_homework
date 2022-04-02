e8e08d53f180ef512916768a5a83ae58522f653e
GreIS93 <grekovis93@gmail.com>
2022-04-02 13:26:12 +0700

    hwtask4

--- homework/task3/Program.cs
+++ homework/task3/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Подсчитать сумму цифр в числе
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число n");
+
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+while (Math.Abs(n) > 0)
+{
+    sum = sum + (n % 10);
+    n = n / 10;
+}
+Console.WriteLine(Math.Abs(sum));
--- homework/task4/Program.cs
+++ homework/task4/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N   
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int S = 1;
+for (int i = 1; i <= n; i++)
+{
+    S = S * i;
+}
+Console.WriteLine(S);
