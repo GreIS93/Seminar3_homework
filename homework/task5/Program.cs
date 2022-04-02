da68c4018b8f100f538f330cc8a19f8572e77523
GreIS93 <grekovis93@gmail.com>
2022-04-02 14:15:52 +0700

    hwtask5

--- homework/task5/Program.cs
+++ homework/task5/Program.cs
@@ -1,2 +1,29 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
-Console.WriteLine("Hello, World!");
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
